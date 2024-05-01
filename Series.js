cb544364800f7f4cff91311c15011b920dacffdb
leonardobarx <leonardobarx@gmail.com>
2024-05-01 14:25:26 -0300

    catalog update

--- Series.js
+++ Series.js
@@ -1,25 +1,28 @@
 const catalogs = [
     { id: "tt14377480", type: "series", name: "Kamen Rider Black Sun", genres: ["Legendado"], poster: null },
+    { id: "tt6982472", type: "series", name: "Kamen Rider Build", genres: ["Legendado"], poster: null },
 ]
 
 
 const seriesStreams = {
-      // Kamen Rider Kuuga
-      "tt0188340:1:1": [{ url: "https://mangas.cloud/tokusatsus/Kamen-Rider/Kuuga/01.mp4", title: "Episódio 1 - mangas.cloud", season: 1, episode: 1 }],
-      "tt0188340:1:2": [{ url: "https://mangas.cloud/tokusatsus/Kamen-Rider/Kuuga/02.mp4", title: "Episódio 2 - mangas.cloud", season: 1, episode: 2 }],
-      "tt0188340:1:3": [{ url: "https://mangas.cloud/tokusatsus/Kamen-Rider/Kuuga/03.mp4", title: "Episódio 3 - mangas.cloud", season: 1, episode: 3 }],
-      // Kamen Rider Black Sun
-      "tt14377480:1:1": [{ url: "https://archive.org/download/08-1080-leg/KRBS-LEG-1080/01%201080%20LEG.mkv", title: "Episódio-1-FHD-Legendado-[New Wave]", season: 1, episode: 1 }],
-      "tt14377480:1:2": [{ url: "https://archive.org/download/08-1080-leg/KRBS-LEG-1080/02%201080%20LEG.mkv", title: "Episódio-2-FHD-Legendado-[New Wave]", season: 1, episode: 2 }],
-      "tt14377480:1:3": [{ url: "https://archive.org/download/08-1080-leg/KRBS-LEG-1080/03%201080%20LEG.mkv", title: "Episódio-3-FHD-Legendado-[New Wave]", season: 1, episode: 3 }],
-      "tt14377480:1:4": [{ url: "https://archive.org/download/08-1080-leg/KRBS-LEG-1080/04%201080%20LEG.mkv", title: "Episódio-4-FHD-Legendado-[New Wave]", season: 1, episode: 4 }],
-      "tt14377480:1:5": [{ url: "https://archive.org/download/08-1080-leg/KRBS-LEG-1080/05%201080%20LEG.mkv", title: "Episódio-5-FHD-Legendado-[New Wave]", season: 1, episode: 5 }],
-      "tt14377480:1:6": [{ url: "https://archive.org/download/08-1080-leg/KRBS-LEG-1080/06%201080%20LEG.mkv", title: "Episódio-6-FHD-Legendado-[New Wave]", season: 1, episode: 6 }],
-      "tt14377480:1:7": [{ url: "https://archive.org/download/08-1080-leg/KRBS-LEG-1080/07%201080%20LEG.mkv", title: "Episódio-7-FHD-Legendado-[New Wave]", season: 1, episode: 7 }],
-      "tt14377480:1:8": [{ url: "https://archive.org/download/08-1080-leg/KRBS-LEG-1080/08%201080%20LEG.mkv", title: "Episódio-8-FHD-Legendado-[New Wave]", season: 1, episode: 8 }],
-      "tt14377480:1:9": [{ url: "https://archive.org/download/08-1080-leg/KRBS-LEG-1080/09%201080%20LEG.mkv", title: "Episódio-9-FHD-Legendado-[New Wave]", season: 1, episode: 9 }],
-      "tt14377480:1:10": [{ url: "https://archive.org/download/08-1080-leg/KRBS-LEG-1080/10%201080%20LEG.mkv", title: "Episódio-10-FHD-Legendado-[New Wave]", season: 1, episode: 10 }]
+     
+// Kamen Rider Black Sun
+      "tt14377480:1:1": [{ url: "https://archive.org/download/08-1080-leg/KRBS-LEG-1080/01%201080%20LEG.mkv", title: "WEB-Episódio-1-FHD-Legendado-[New Wave]", season: 1, episode: 1 }],
+      "tt14377480:1:2": [{ url: "https://archive.org/download/08-1080-leg/KRBS-LEG-1080/02%201080%20LEG.mkv", title: "WEB-Episódio-2-FHD-Legendado-[New Wave]", season: 1, episode: 2 }],
+      "tt14377480:1:3": [{ url: "https://archive.org/download/08-1080-leg/KRBS-LEG-1080/03%201080%20LEG.mkv", title: "WEB-Episódio-3-FHD-Legendado-[New Wave]", season: 1, episode: 3 }],
+      "tt14377480:1:4": [{ url: "https://archive.org/download/08-1080-leg/KRBS-LEG-1080/04%201080%20LEG.mkv", title: "WEB-Episódio-4-FHD-Legendado-[New Wave]", season: 1, episode: 4 }],
+      "tt14377480:1:5": [{ url: "https://archive.org/download/08-1080-leg/KRBS-LEG-1080/05%201080%20LEG.mkv", title: "WEB-Episódio-5-FHD-Legendado-[New Wave]", season: 1, episode: 5 }],
+      "tt14377480:1:6": [{ url: "https://archive.org/download/08-1080-leg/KRBS-LEG-1080/06%201080%20LEG.mkv", title: "WEB-Episódio-6-FHD-Legendado-[New Wave]", season: 1, episode: 6 }],
+      "tt14377480:1:7": [{ url: "https://archive.org/download/08-1080-leg/KRBS-LEG-1080/07%201080%20LEG.mkv", title: "WEB-Episódio-7-FHD-Legendado-[New Wave]", season: 1, episode: 7 }],
+      "tt14377480:1:8": [{ url: "https://archive.org/download/08-1080-leg/KRBS-LEG-1080/08%201080%20LEG.mkv", title: "WEB-Episódio-8-FHD-Legendado-[New Wave]", season: 1, episode: 8 }],
+      "tt14377480:1:9": [{ url: "https://archive.org/download/08-1080-leg/KRBS-LEG-1080/09%201080%20LEG.mkv", title: "WEB-Episódio-9-FHD-Legendado-[New Wave]", season: 1, episode: 9 }],
+      "tt14377480:1:10": [{ url: "https://archive.org/download/08-1080-leg/KRBS-LEG-1080/10%201080%20LEG.mkv", title: "WEB-Episódio-10-FHD-Legendado-[New Wave]", season: 1, episode: 10 }],
 
+//Kamen Rider Build
+       "tt6982472:1:1": [{ url: "https://archive.org/download/kr-b-pt-01/KR-B-PT%2001.mkv", title: "WEB-Episódio-1-Legendado-[New Wave]", season: 1, episode: 1 }],
+       "tt6982472:1:2": [{ url: "https://archive.org/download/kr-b-pt-02/KR-B-PT%2002.mkv", title: "WEB-Episódio-2-Legendado-[New Wave]", season: 1, episode: 2 }],
+       "tt6982472:1:3": [{ url: "https://archive.org/download/kr-b-pt-03/KR-B-PT%2003.mkv", title: "WEB-Episódio-3-Legendado-[New Wave]", season: 1, episode: 3 }],
+       "tt6982472:1:4": [{ url: "https://archive.org/download/kr-b-pt-04/KR-B-PT%2004.mkv", title: "WEB-Episódio-3-Legendado-[New Wave]", season: 1, episode: 4 }],
     };
 
 
