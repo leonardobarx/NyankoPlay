10b9027261203f44d55d713219117f90e5c9c7de
leonardobarx <leonardobarx@gmail.com>
2024-05-01 01:06:49 -0300

    update movies

--- Movies.js
+++ Movies.js
@@ -4,7 +4,7 @@ const catalogs = [
 
 
 const movieStreams = {
-    // Adicione seus streams de filmes aqui
+    "tt0156674": [{ url: "https://archive.org/download/kamen-rider-super-1-movie/%5BNonActionableFansubs%5DKamen.Rider.Super-1.Movie.1981.2160p.Bluray.REMUX.h265.HDR10.PCM.2.0%5BE69D6760%5D.mp4", title: "Kamen Rider Super-1: The Movie", season: 1, episode: 1 }],
 };
 
 module.exports = {
