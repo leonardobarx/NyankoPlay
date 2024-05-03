bcea53d2ae5adadb77f25bdbd2196601b81ff2a6
leonardobarx <leonardobarx@gmail.com>
2024-05-03 20:26:09 -0300

    Update Movies.js

--- Movies.js
+++ Movies.js
@@ -2,7 +2,7 @@ const catalogs = [
    { id: "tt14379088", type: "movie", name: "Shin Kamen Rider", genres: ["Legendado"], poster: null },
 ]
 const movieStreams = {
-    "tt14379088": [{ url: "https://archive.org/download/shin.-kamen.-rider.-2023.1080p.-amzn.-web-dl.-ddp-5.1.-h.-264-magic-star/Shin.Kamen.Rider.2023.1080p.AMZN.WEB-DL.DDP5.1.H.264-MagicStar.mkv", title: "Shin Kamen Rider - web", season: 1, episode: 1 }],
+    "tt14379088": [{ infoHash: "17a10d72c9980d67e338f34bcfc87ea9519dfb48", title: "Shin Kamen Rider - web", season: 1, episode: 1 }],
 };
 
 const movieSubtitles = {
