2fb575be6836d6a09dda1c3328df8f5aa03ce5ae
leonardobarx <leonardobarx@gmail.com>
2024-05-03 20:26:55 -0300

    Update Movies.js

--- Movies.js
+++ Movies.js
@@ -1,8 +1,8 @@
 const catalogs = [
-   { id: "tt14379088", type: "movie", name: "Shin Kamen Rider", genres: ["Legendado"], poster: null },
+   { id: "tt14379088", type: "movie", name: "Shin Kamen Rider 2023", genres: ["Legendado"], poster: null },
 ]
 const movieStreams = {
-    "tt14379088": [{ infoHash: "17a10d72c9980d67e338f34bcfc87ea9519dfb48", title: "Shin Kamen Rider - web", season: 1, episode: 1 }],
+    "tt14379088": [{ infoHash: "17a10d72c9980d67e338f34bcfc87ea9519dfb48", title: "Shin Kamen Rider - Torrent - Nyaa", season: 1, episode: 1 }],
 };
 
 const movieSubtitles = {
